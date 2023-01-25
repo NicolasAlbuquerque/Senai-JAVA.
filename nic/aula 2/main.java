ede22676b4fa78b4aa83c4709182138cc0ad2336
Nicolas Albuquerque <contato.nicolasalbuquerque@gmail.com>
2023-01-25 10:06:57 -0300

    Organizado os exercícios feitos em sala.

--- nic/aula 2/main.java
+++
@@ -1,8 +0,0 @@
-public class canstants {
-}
-
-    public static void main (String [] args) {
-        public static final double GRAVIDADE =32;
-        System.out.println("A Gravidade é " +Gravidade);
-
-    }
